b8fc97694b27be32f3ed1c6b8473ceca4f8ac0ed
Ignacio <ignaciotejada87@gmail.com>
2024-02-19 18:36:39 +0100

    19/02/2024 2

--- src/app/components/industry-chart/industry-chart.component.ts
+++ src/app/components/industry-chart/industry-chart.component.ts
@@ -1,11 +1,11 @@
-import { Component, ElementRef, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
-import { Chart } from 'chart.js/auto';
-import { ContenidoComponent } from '../contenido/contenido.component';
+import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-industry-chart',
   templateUrl: './industry-chart.component.html',
 })
 export class IndustryChartComponent {
- 
+
+
 }
